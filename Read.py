4d17a746c84584e39a484e80e6d119bb9f2d1b8d
tdos233 <tdos233@gmail.com>
2018-07-05 21:09:05 -0300

    terminado o carregamento de dados do csv

--- Read.py
+++ Read.py
@@ -137,20 +137,31 @@ def select_acidente(u):
 def carregar_csv():
     idx = indexs()
     Id_local=1
+    tipoveiculo=['PASSENGER VEHICLE', 'BICYCLE', 'PICK-UP TRUCK', 'SPORT UTILITY / STATION WAGON', 'TAXI', 'MOTORCYCLE', 'FIRE TRUCK', 'VAN', 'PEDICAB', 'AMBULANCE', 'BUS', 'UNKNOWN', 'SCOOTER', 'OTHER', 'SMALL COM VEH(4 TIRES) ', 'LARGE COM VEH(6 OR MORE TIRES)', 'LIVERY VEHICLE']
+    fatortipo=['Aggressive Driving/Road Rage', 'Driver Inexperience', 'Other Vehicular', 'Unspecified' , 'Driver Inattention/Distraction', 'Alcohol Involvement', 'Failure to Yield Right-of-Way', 'Cell Phone (hand-held)', 'Unsafe Lane Changing', 'Turning Improperly', 'Following Too Closely', 'Backing Unsafely', 'Passenger Distraction', 'Reaction to Other Uninvolved Vehicle', 'Unsafe Speed', 'Pedestrian/Bicyclist/Other Pedestrian Error/Confusion', 'Traffic Control Disregarded', 'Passing or Lane Usage Improper', 'Outside Car Distraction', 'Obstruction/Debris', 'Failure to Keep Right', 'Animals Action', 'Oversized Vehicle', 'Illness', 'View Obstructed/Limited', 'Lost Consciousness', 'Tire Failure/Inadequate', 'Glare', 'Lane Marking Improper/Inadequate', 'Driverless/Runaway Vehicle', 'Fatigued/Drowsy', 'Traffic Control Device Improper/Non-Working', 'Fell Asleep', 'Other Electronic Device', 'Brakes Defective', 'Other Lighting Defects', 'Pavement Slippery', 'Pavement Defective', 'Steering Failure', 'Headlights Defective', 'Drugs (Illegal)', 'Accelerator Defective', 'Prescription Medication', 'Physical Disability', 'Tow Hitch Defective', 'Cell Phone (hands-free)', 'Shoulders Defective/Improper', 'Windshield Inadequate']
     InsertTipoPessoa(1,"Pessoa")
     InsertTipoPessoa(2,"Pedestre")
     InsertTipoPessoa(3,"Ciclista")
     InsertTipoPessoa(4,"Motorista")
-    InsertTipoVeiculo()
+    for index, item in enumerate(tipoveiculo):
+        InsertTipoVeiculo(index+1,item)
+    for index1, item1 in enumerate(fatortipo):
+        InsertFator(index1+1,item1)
+
     for rowIndex, row in enumerate(read_data('/home/thiago/Documentos/bd/NYPD_Motor_Vehicle_Collisions.csv')):
         InsertIntoAcidente(row[idx.date], row[idx.time], row[idx.uniqueKey])
         InsertIntoLocalidade(Id_local,row[borough],row[ZipCode],row[latitude],row[longitude])
         Id_local+=1
-        InsertTipoVeiculo()
-        InsertFator()
-        InsertAcidenteLocal()
-        InsertAcidentePessoa()
-        InsertAcidenteVeiculo()
+        InsertAcidenteLocal(Id_local,row[idx.uniqueKey],row[idx.onStreetName],row[idx.offStreetName],row[idx.crossStreetName])
+        InsertAcidentePessoa(1,row[idx.numberOfPersonsInjured],row[idx.numberOfPersonsKilled],row[idx.uniqueKey])
+        InsertAcidentePessoa(2,row[idx.numberOfPedestriansInjured],row[idx.numberOfPedestriansKilled],row[idx.uniqueKey])
+        InsertAcidentePessoa(3,row[idx.numberOfCyclistInjured],row[idx.numberOfCyclistKilled],row[idx.uniqueKey])
+        InsertAcidentePessoa(4,row[idx.numberOfMotoristInjured],row[idx.numberOfMotoristKilled],row[idx.uniqueKey])
+        InsertAcidenteVeiculo(tipoveiculo.index(row[idx.vehicleTypeCode1]),fatortipo.index(row[idx.contributingFactorVehicle1]),row[idx.uniqueKey])
+        InsertAcidenteVeiculo(tipoveiculo.index(row[idx.vehicleTypeCode2]),fatortipo.index(row[idx.contributingFactorVehicle2]),row[idx.uniqueKey])
+        InsertAcidenteVeiculo(tipoveiculo.index(row[idx.vehicleTypeCode3]),fatortipo.index(row[idx.contributingFactorVehicle3]),row[idx.uniqueKey])
+        InsertAcidenteVeiculo(tipoveiculo.index(row[idx.vehicleTypeCode4]),fatortipo.index(row[idx.contributingFactorVehicle4]),row[idx.uniqueKey])
+        InsertAcidenteVeiculo(tipoveiculo.index(row[idx.vehicleTypeCode5]),fatortipo.index(row[idx.contributingFactorVehicle5]),row[idx.uniqueKey])
         
 # -------------------------------------------------------
 
